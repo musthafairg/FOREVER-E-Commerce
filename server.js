2a6808bd07460cc3d3b5154865ad3cf6e39431b0
musthafairg <musthafairg786@gmail.com>
2025-12-04 22:17:16 +0530

    Fix Render server port binding

--- server.js
+++ server.js
@@ -28,10 +28,13 @@ app.use(session({
     },
 }))
 
+
+
+
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
