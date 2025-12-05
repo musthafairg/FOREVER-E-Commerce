8932e45798f2c1320bf942fe96dadb2bf4f8a8b2
musthafairg <musthafairg786@gmail.com>
2025-12-05 12:08:46 +0530

    error message displaying updated

--- config/passport.js
+++ config/passport.js
@@ -7,8 +7,8 @@ import 'dotenv/config'
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'http://localhost:3000/auth/google/callback'
-   //callbackURL: 'https://forever-e-commerce-1.onrender.com/auth/google/callback'
+   // callbackURL:'http://localhost:3000/auth/google/callback'
+   callbackURL: 'https://forever-e-commerce-1.onrender.com/auth/google/callback'
 },
 
 async (accessToken,refreshToken,profile,done)=>{
